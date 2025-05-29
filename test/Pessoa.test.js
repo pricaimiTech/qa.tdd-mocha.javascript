43331bfe26abf1aa9a0d10fcc90ef3307b4fdf40
Priscila Caimi <123993547+pricaimiTech@users.noreply.github.com>
2025-05-29 11:26:42 -0300

    Update Pessoa.test.js
    atualizando cenários

--- test/Pessoa.test.js
+++ test/Pessoa.test.js
@@ -1,5 +1,7 @@
-import { expect } from "chai";
-import Pessoa from "../src/Pessoa.js";
+const {
+    expect
+} = require('chai')
+const Pessoa = require('../src/Pessoa.js');
 
 describe("Validação de idade para consumo de álcool", () => {
 
@@ -10,18 +12,18 @@ describe("Validação de idade para consumo de álcool", () => {
     })
 
     describe('RF001: Pessoas maiores de 17 anos podem consumir bebida alcoólica', () => {
-        it('[CT-OO1] - Validar que pessoa com Idade IGUAL a 17 anos pode consumir bebida álcoolica', () => {
-            const result = pessoaMaiorIdade.ehMaiorDeIdade(17);
-            expect(result).to.be.true;
-        });
-
-        it('[CT-OO2] - Validar que pessoa com Idade MAIOR que 17 anos pode consumir bebida álcoolica', () => {
+        it('[CT-OO1] - Validar que pessoa com Idade MAIOR que 17 anos pode consumir bebida álcoolica', () => {
             const result = pessoaMaiorIdade.ehMaiorDeIdade(18);
             expect(result).to.be.true;
         });
     });
 
     describe('RF002: Pessoas menores de 17 anos não podem consumir bebida alcoólica.', () => {
+        it('[CT-OO2] - Validar que pessoa com Idade IGUAL a 17 anos não pode consumir bebida álcoolica', () => {
+            const result = pessoaMaiorIdade.ehMaiorDeIdade(17);
+            expect(result).to.be.false;
+        });
+
         it('[CT-OO3] - Validar que pessoa com Idade MENOR a 17 anos não pode consumir bebida álcoolica', () => {
             const result = pessoaMaiorIdade.ehMaiorDeIdade(16);
             expect(result).to.be.false;
@@ -29,8 +31,9 @@ describe("Validação de idade para consumo de álcool", () => {
     });
 
     describe('Validar possíveis erros de inserção de dados inválidos pelo usuário', () => {
+
         it('[CT-OO4] - Validar que o sistema não deve aceitar idade negativas', () => {
-            const idade = -1 
+            const idade = -1
             expect(() => {
                 pessoaMaiorIdade.ehMaiorDeIdade(idade);
             }).to.throw(Error, `Idade informada: ${idade} anos. Não é permitido idade negativa`);
