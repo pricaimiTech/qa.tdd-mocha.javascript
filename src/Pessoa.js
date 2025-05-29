41ed65631f7de1bb7bb1fa33e825f03702ab0344
Priscila Caimi <priscila.caimi@hotmail.com>
2025-05-28 23:14:25 -0300

    implementando melhoria no tratamento de erro e adicionando documentação ao código

--- src/Pessoa.js
+++ src/Pessoa.js
@@ -1,8 +1,18 @@
-// src/Pessoa.js
+/**
+ * @description Classe Pessoa com método para verificar se a pessoa é maior de idade.
+ * @class Pessoa
+ */
 export default class Pessoa {
+    /**
+     * 
+     * @param {*} idade 
+     * @returns {boolean} Retorna true se a idade for maior ou igual a 17, caso contrário retorna false.
+     * @throws {Error} Lança um erro se a idade não for um número inteiro positivo.
+     * @description Método para verificar se a pessoa é maior de idade (17 anos ou mais).
+     */
   ehMaiorDeIdade(idade) {
-    if (typeof idade !== 'number' || idade < 0) {
-      throw new Error('Idade deve ser um número positivo');
+    if (!Number.isInteger(idade) || idade < 0) {
+      throw new Error(`Idade informada: ${idade} anos. Deve ser um número inteiro e positivo`);
     }
     
     return idade >= 17;
